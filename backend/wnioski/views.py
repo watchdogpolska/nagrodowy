7bb3dd4a17479650ceb0165ef1433fdedd1a14c8
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-07-05 00:40:42 +0200

    Clean up views.py

--- backend/wnioski/views.py
+++ backend/wnioski/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
-from models import Adresat
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
 from models import Wniosek, WniosekZalacznik
 import json
 
@@ -17,25 +16,15 @@ def array_to_json(arr, callback):
 
 
 def get_map_markers(request, callback):
-    recs = Wniosek.objects.all()
-
-    arr = []
-    for wniosek in recs:
-        adr = wniosek.adresat
-        if (adr.szerokosc_geo != None and adr.dlugosc_geo != None) or adr.miasto != None:
-            arr.append(wniosek.get_map_marker())
-
-    return array_to_json(arr, callback)
+    filters = Q(Q(adresat__szerokosc_geo__isnull=False) & Q(adresat__dlugosc_geo__isnull=False)) | \
+                    Q(adresat__miasto__isnull=False)
+    recs = Wniosek.objects.filter(filters).all()
+    return array_to_json((x.get_map_marker() for x in recs), callback)
 
 
 def get_marker_details(request, markerId, callback):
-    recs = Wniosek.objects.filter(id=markerId)
-    markerDetails = recs[0].get_map_marker_details()
-
-    recs = WniosekZalacznik.objects.all().filter(wniosek=markerId)
-    filesDetails = [obj.get_as_obj() for obj in recs]
-
-    data = {'marker': markerDetails,
-            'files': filesDetails}
+    data = {'marker': get_object_or_404(Wniosek, pk=markerId).get_map_marker_details(),
+            'files':  (obj.get_as_obj()
+                       for obj in WniosekZalacznik.objects.filter(wniosek=markerId))}
 
     return array_to_json(data, callback)
