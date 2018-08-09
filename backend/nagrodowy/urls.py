3b9f1b086ab4507a628658d08cd1b4e10f0ec344
Krzysztof Biernat <krzysztof@biernat.in>
2018-08-09 20:07:44 +0200

    redirect index

--- backend/nagrodowy/urls.py
+++ backend/nagrodowy/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, patterns, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 admin.autodiscover()
 
@@ -8,4 +9,5 @@ urlpatterns = patterns('',
 
     # admin
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', RedirectView.as_view(url='https://siecobywatelska.pl')),
 )
